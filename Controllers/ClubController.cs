fbc0d8fd0999e9902831342575b85afa4df37b49
Prog <vyachhw@gmail.com>
2023-03-01 20:41:47 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+using System.Data.Entity;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+
+            return View(clubs);
+        }
+        public IActionResult Detail(int id)
+        {
+            Club? club = _context.Clubs.Include(o=>o.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
+        }
+    }
+}
---
+++ Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(context.Races.ToList());
+        }
+    }
+}
